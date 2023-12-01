94a2906b71c124fa050c94890378fd60bf96dce1
Tchokaklian <pascal.tchokaklian@atos.net>
2023-12-01 16:40:56 +0100

    le ménage

--- django_projects/locallibrary/catalog/urls.py
+++
@@ -1,8 +0,0 @@
-from os import path
-
-from django import views
-
-
-urlpatterns = [
-    path('locallibrary', views.index, name='index'),
-]
--- tchok.com/sauvegarde/20231027/envvar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $dbHost = 'tchokcommcdbo100.mysql.db';
-    $dbBase = 'tchokcommcdbo100';
-    $dbUser = 'tchokcommcdbo100';
-    $dbPassword = 'Sarkis19';
-?>
-
